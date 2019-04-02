3edc6ff336b8367caab94b90f214e87e75d7d67f
Chris Yang <cyang287@yahoo.com>
2019-04-02 16:53:34 -0400

    finished html markup

---
+++ index.php
@@ -0,0 +1,405 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+	<title>Gatlinburg, TN</title>
+	<meta name="description" content="">
+	<meta name="robots" content="index, follow">
+
+	<link rel="stylesheet" href="_assets_/plugins/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" href="_assets_/fonts/font-awesome/css/font-awesome.min.css">
+	<link rel="stylesheet" href="_assets_/plugins/owl.carousel/owl-carousel/owl.carousel.css">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Merriweather:400i|Roboto:700">
+	<link rel="stylesheet" href="_assets_/css/layout.css">
+
+	<link rel="shortcut icon" href="_assets_/images/favicon.ico">
+	<link rel="apple-touch-icon" href="_assets_/images/touch-icon-iphone.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="_assets_/images/touch-icon-ipad.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="_assets_/images/touch-icon-iphone4.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="_assets_/images/touch-icon-ipad2.png">
+
+	<!--[if !IE]><!-->
+	<link rel="stylesheet" href="_assets_/plugins/add-to-homescreen/style/addtohomescreen.css">
+	<script src="_assets_/plugins/add-to-homescreen/src/addtohomescreen.min.js"></script>
+	<script>addToHomescreen();</script>
+	<!--<![endif]-->
+
+	<!-- Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!--[if lt IE 9]>
+	  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+
+</head>
+<body id="homepage">
+
+<header class="clearfix">
+	<a href="#main" id="skip" tabindex="0">SKIP TO CONTENT</a>
+
+	<div class="container">
+		<a href="./" id="logo">
+			<img src="./_assets_/images/logo.png" alt="Logo">
+		</a><!-- /.logo -->
+
+		<div id="toggles" class="hidden-lg hidden-md">
+			<div tabindex="0" id="search-toggle-mobile" class="fa fa-search"></div>
+			<div tabindex="0" id="nav-toggle" class="fa fa-bars"></div>	
+		</div><!-- /#toggles -->
+
+		<nav>
+			<ul id="nav">
+				<li><a href="./">home</a></li>
+				<li>
+					<a href="./">departments</a>
+					<ul>
+						<li><a href="./">building inspections</a></li>
+						<li><a href="./">building maintenance</a></li>
+						<li>
+							<a href="./">convention center</a>
+							<ul>
+								<li><a href="./">this is a link</a></li>
+								<li><a href="./">this is a link</a></li>
+								<li><a href="./">this is a link</a></li>
+								<li><a href="./">this is a link</a></li>
+								<li><a href="./">this is a link</a></li>
+							</ul>
+						</li>
+						<li><a href="./">finance</a></li>
+						<li><a href="./">fire</a></li>
+						<li><a href="./">golf</a></li>
+						<li><a href="./">mass transit</a></li>
+						<li><a href="./">parking</a></li>
+						<li><a href="./">planning</a></li>
+						<li><a href="./">police</a></li>
+						<li><a href="./">recreation</a></li>
+						<li><a href="./">sanitation</a></li>
+						<li><a href="./">service center</a></li>
+						<li><a href="./">street</a></li>
+						<li><a href="./">utilities</a></li>
+					</ul>
+				</li>
+				<li><a href="./">residents</a></li>
+				<li><a href="./">visitors</a></li>
+				<li><a href="./">government</a></li>
+				<li>
+					<a href="./">i want to...</a>
+					<ul>
+						<li><a href="./">aenean eu leo quam</a></li>
+						<li><a href="./">pellentesque ornare</a></li>
+						<li><a href="./">sem lacinia quam</a></li>
+						<li><a href="./">venenatis vestibulum</a></li>
+						<li><a href="./">cras justo odio</a></li>
+						<li><a href="./">dapibus ac facilisis</a></li>
+						<li><a href="./">in, egestas eget</a></li>
+						<li><a href="./">aenean eu leo</a></li>
+						<li><a href="./">pellentesque</a></li>
+						<li><a href="./">lacinia quam</a></li>
+						<li><a href="./">vestibulum. lor</a></li>
+						<li><a href="./">dolor sit amet</a></li>
+					</ul>
+				</li>
+			</ul> <!-- /#nav -->
+		</nav>
+
+		<div class="social-links hidden-sm hidden-xs">
+			<a href="./" class="fa fa-twitter"></a>
+			<a href="./" class="fa fa-facebook"></a>
+		</div><!-- /.social-links -->
+
+		<div id="search">
+			<form class="search-form" method="get" action="search.php">
+				<label class="fa fa-search" for="search-input"></label>
+				<input name="q" class="form-control search-input" placeholder="Enter search terms..." type="search" id="search-input">
+				<button><i class="fa fa-search"></i></button>
+			</form><!-- /.search-form -->
+		</div><!-- /#search -->
+	</div><!-- /.container -->
+</header><!-- /.clearfix -->
+
+<section id="slider">
+	<ul class="bxslider">
+		<li style="background: url('./_assets_/images/hero1.jpg') center no-repeat;background-size:cover;"></li>
+		<li style="background: url('./_assets_/images/hero2.jpg') center no-repeat;background-size:cover;"></li>
+		<li style="background: url('./_assets_/images/hero3.jpg') center no-repeat;background-size:cover;"></li>
+	</ul><!-- /.bxslider -->
+
+	<div class="container">
+		<h1><span>the gateway to the</span> great smoky mountains</h1>
+		<a href="./" class="btn">explore</a>
+	</div><!-- /.container -->
+	<div id="hero-whitespace" style="background:url('./_assets_/images/hero-whitespace.png') center no-repeat;background-size:cover;"></div>
+</section><!-- /#slider -->
+
+<main id="main">
+	<section id="content" style="background:url('./_assets_/images/content-bg.jpg') center no-repeat;background-size:cover;">
+		<section id="welcome">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-6">
+						<h2><span>an adventrue like no other</span> welcome</h2>
+						<a href="./">read more</a>
+						<div class="img-container">
+							<div class="welcome-img" style="background:url('./_assets_/images/welcome2.jpg') center no-repeat;background-size:cover;"></div>
+							<div class="welcome-img" style="background:url('./_assets_/images/welcome3.jpg') center no-repeat;background-size:cover;"></div>
+							<span>- caption here</span>
+						</div><!-- /.img-container -->
+					</div><!-- /.col-md-6 -->
+					
+					<div class="col-md-6">
+						<div class="welcome-img" style="background:url('./_assets_/images/welcome1.jpg') center no-repeat;background-size:cover;"></div>
+						<span>- caption here</span>
+					</div><!-- /.col-md-6 -->
+				</div><!-- /.row -->
+			</div><!-- /.container -->
+		</section><!-- /#welcome -->
+
+		<section id="quick-links">
+			<div id="quick-links-bg" style="background:url('./_assets_/images/ql-bg.png') center no-repeat;background-size:cover;"></div>
+			<div class="container">
+				<div class="row">
+					<div class="img-container col-md-6">
+						<div class="quick-link-img" style="background:url('./_assets_/images/ql-img1.jpg') center no-repeat;background-size:cover;"></div>
+						<div class="quick-link-img" style="background:url('./_assets_/images/ql-img2.jpg') center no-repeat;background-size:cover;"></div>
+					</div><!-- /.img-container.col-md-6 -->
+
+					<div id="quick-links-wrapper" class="col-md-5 col-md-offset-1">
+						<a href="./" class="quick-link">
+							<div class="quick-link-icon" style="background:url('./_assets_/images/ql1.png') center no-repeat;background-size:cover;"></div>
+							<h3>document center</h3>
+							<span>view</span>
+						</a><!-- /.quick-links -->
+						<a href="./" class="quick-link">
+							<div class="quick-link-icon" style="background:url('./_assets_/images/ql2.png') center no-repeat;background-size:cover;"></div>
+							<h3>economic development</h3>
+							<span>view</span>
+						</a><!-- /.quick-links -->
+						<a href="./" class="quick-link">
+							<div class="quick-link-icon" style="background:url('./_assets_/images/ql3.png') center no-repeat;background-size:cover;"></div>
+							<h3>connect tennessee</h3>
+							<span>view</span>
+						</a><!-- /.quick-links -->
+						<a href="./" class="quick-link">
+							<div class="quick-link-icon" style="background:url('./_assets_/images/ql4.png') center no-repeat;background-size:cover;"></div>
+							<h3>flood information</h3>
+							<span>view</span>
+						</a><!-- /.quick-links -->
+					</div><!-- /#quick-links-wrapper.owl-carousel -->
+				</div><!-- /.row -->
+			</div><!-- /.container -->
+		</section><!-- /#quick-links -->
+
+		<section id="news-events">
+			<div class="container">
+				<div class="row">
+					<section id="news" class="col-md-8">
+						<div id="news-circle" style="background:url('./_assets_/images/news-circle.png') center no-repeat;background-size:cover;"></div>
+						<h2><span>stay up to date</span>latest news</h2>
+						<span>featured news</span>
+						<div id="news-links" class="owl-carousel">
+							<div class="news-link">
+								<div class="news-link-img" style="background:url('./_assets_/images/news1.jpg') center no-repeat;background-size:cover;"></div>
+								<div class="news-link-info">
+									<span>march 11 2019</span>
+									<h3>cursus lorem inceptos consectetur et. fringilla magna commodo</h3>
+									<a href="./">read more</a>
+								</div><!-- /.news-link-info -->
+							</div><!-- /.news-link -->
+							<div class="news-link">
+								<div class="news-link-img" style="background:url('./_assets_/images/news2.jpg') center no-repeat;background-size:cover;"></div>
+								<div class="news-link-info">
+									<span>march 5 2019</span>
+									<h3>donec id elit non mi porta gravida at eget metus</h3>
+									<p>
+										Nulla vitae elit libero, a pharetra augue. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum. Lorem ipsum dolor sit amet,
+									</p>
+									<a href="./">read more</a>
+								</div><!-- /.news-link-info -->
+							</div><!-- /.news-link -->
+							<div class="news-link">
+								<div class="news-link-img" style="background:url('./_assets_/images/news3.jpg') center no-repeat;background-size:cover;"></div>
+								<div class="news-link-info">
+									<span>march 2 2019</span>
+									<h3>duis mollis, est non commodo luctus, nisi erat porttitor</h3>
+									<p>
+										Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Aenean lacinia bibendum nulla sed consectetur. Etiam porta sem malesuada
+									</p>
+									<a href="./">read more</a>
+								</div><!-- /.news-link-info -->
+							</div><!-- /.news-link -->
+						</div><!-- /#news-links -->
+						<a href="./" class="btn">all news</a>
+					</section><!-- /#news -->
+
+					<section id="events" class="col-md-4">
+						<h2>upcoming events</h2>
+						<img src="./_assets_/images/events.png" alt="Events">
+						<a href="./" class="btn">all events</a>
+					</section><!-- /#events -->
+				</div><!-- /.row -->
+			</div><!-- /.container -->
+		</section><!-- /#news-events -->
+
+		<section id="alerts">
+			<div id="alerts-bg" style="background:url('./_assets_/images/alerts-bg.jpg') center no-repeat;background-size:cover;"></div>
+			<div id="alerts-whitespace" style="background:url('./_assets_/images/alerts-whitespace.jpg') center no-repeat;background-size:cover;"></div>
+			<div class="img-container">
+				<div class="alerts-img" style="background:url('./_assets_/images/alerts1.jpg') center no-repeat;background-size:cover;"></div>
+				<div class="alerts-img" style="background:url('./_assets_/images/alerts2.jpg') center no-repeat;background-size:cover;"></div>
+				<div class="alerts-img" style="background:url('./_assets_/images/alerts3.jpg') center no-repeat;background-size:cover;"></div>
+				<div class="alerts-img" style="background:url('./_assets_/images/alerts4.jpg') center no-repeat;background-size:cover;"></div>
+				<div class="alerts-img" style="background:url('./_assets_/images/alerts5.jpg') center no-repeat;background-size:cover;"></div>
+				<div class="alerts-img" style="background:url('./_assets_/images/alerts6.jpg') center no-repeat;background-size:cover;"></div>
+			</div><!-- /.img-container -->
+
+			<div class="container">
+				<form>
+					<label for="signup-input"><span>stay in the loop</span>emergency alerts sign up</label>
+					<input name="q" class="form-control" placeholder="Your email@email.com" id="signup-input">
+					<button class="btn">subscribe</button>
+					<span>spam is no excepted</span>
+				</form>
+			</div><!-- /.container -->
+		</section><!-- /#alerts -->
+	</section><!-- /#content -->
+</main>
+
+<footer>
+	<section id="top-footer">
+		<div class="container">
+			<section id="footer-links">
+				<div id="department-links">
+					<ul>
+						<li><a href="./">building inspections</a></li>
+						<li><a href="./">building maintenance</a></li>
+						<li><a href="./">convention center</a></li>
+						<li><a href="./">finance</a></li>
+						<li><a href="./">fire</a></li>
+						<li><a href="./">golf</a></li>
+						<li><a href="./">mass transit</a></li>
+						<li><a href="./">parking</a></li>
+						<li><a href="./">planning</a></li>
+						<li><a href="./">police</a></li>
+						<li><a href="./">recreation</a></li>
+						<li><a href="./">sanitation</a></li>
+						<li><a href="./">service center</a></li>
+						<li><a href="./">street</a></li>
+						<li><a href="./">utilities</a></li>
+					</ul>
+				</div>
+
+				<div id="i-want-to">
+					<ul>
+						<li><a href="./">aenean eu leo quam</a></li>
+						<li><a href="./">pellentesque ornare</a></li>
+						<li><a href="./">sem lacinia quam</a></li>
+						<li><a href="./">venenatis vestibulum</a></li>
+						<li><a href="./">cras justo odio</a></li>
+						<li><a href="./">dapibus ac facilisis</a></li>
+						<li><a href="./">in, egestas eget</a></li>
+						<li><a href="./">aenean eu leo</a></li>
+						<li><a href="./">pellentesque</a></li>
+						<li><a href="./">lacinia quam</a></li>
+						<li><a href="./">vestibulum. lor</a></li>
+						<li><a href="./">dolor sit amet</a></li>
+					</ul>
+				</div>
+			</section><!-- /#footer-links -->
+
+			<section id="contact">
+				<div style="background:url('./_assets_/images/logo.png') center no-repeat;background-size:cover;"></div>
+				<h4>contact city hall</h4>
+				<span>1230 East Parkway<br>Gatlinburg, TN 37738<br>(865) 436-1400</span>
+			</section><!-- /.#contact -->
+		</div><!-- /.container -->
+	</section><!-- /#top-footer -->
+		
+	<section id="revize-logo">
+		<div class="container">
+			<span>&copy; 2019 Gatlinburg TN. All Rights reserved.</span>
+			<span>Powered by <a href="https://www.revize.com/" id="revize-logo">revize</a>, the Government Website Experts.</span>
+			<a href="./">login</a>
+		</div><!-- /.container -->
+	</section><!-- /#revize-logo -->
+</footer>
+
+<div class="alert alert-dismissible floating-alert fade in os-animation" data-os-animation="fadeInUpBig" data-os-animation-delay="2s" role="alert">
+	<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+	<h4>Alert Title 30 characters Max</h4>
+	<p>Max 120 characters Max 120 characters Max 120 characters Max 120 characters Max 120 characters Max 120 characters Max 120</p>
+	<p><a href="#" class="btn">Read More</a></p>
+</div><!-- /.alert -->
+
+
+<!-- Share widget make into an include file -->
+<button type="button" class="share-btn floating-share-btn" data-toggle="modal" data-target="#shareModal">
+	<i class="fa fa-share-alt"></i>
+	<span>Share</span>
+</button>
+
+<div class="modal fade" id="shareModal" tabindex="-1" role="dialog" aria-labelledby="shareModal">
+	<div class="modal-dialog modal-lg" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+				<h4 class="modal-title" id="myModalLabel">Share this page</h4>
+			</div>
+			<div class="modal-body">
+				<div class="copylink">
+					<p>Copy and paste this code into your website.</p>
+					<pre>&lt;a href="<?php echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; ?>">Your Link Name&lt;/a&gt;</pre>
+				</div><!-- /.copylink -->
+				<div class="share-btns">
+					<p>Share this page on your favorite Social network</p>
+					<div class="row">
+						<div class="col-md-3 col-xs-6">
+							<a href="https://www.facebook.com/sharer/sharer.php?u=<?php echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; ?>" class="btn-facebook" onclick="return !window.open(this.href, 'facebook ', 'width=500,height=500')"
+							target="_blank">
+								<i class="fa fa-facebook"></i> Facebook
+							</a>
+						</div>
+						<div class="col-md-3 col-xs-6">
+							<a href="https://www.twitter.com/home?status=<?php echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; ?>" class="btn-twitter" onclick="return !window.open(this.href, 'twitter ', 'width=500,height=500')"
+							target="_blank">
+								<i class="fa fa-twitter"></i> Twitter
+							</a>
+						</div>
+						<div class="col-md-3 col-xs-6">
+							<a href="https://www.plus.google.com/share?url=<?php echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; ?>" class="btn-google" onclick="return !window.open(this.href, 'google ', 'width=500,height=500')"
+							target="_blank">
+								<i class="fa fa-google-plus"></i> Google Plus
+							</a>
+						</div>
+						<div class="col-md-3 col-xs-6">
+							<a href="https://www.reddit.com/submit?url=<?php echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; ?>" class="btn-reddit" onclick="return !window.open(this.href, 'redit ', 'width=500,height=500')"
+							target="_blank">
+								<i class="fa fa-reddit"></i> Reddit
+							</a>
+						</div>
+					</div><!-- /.row -->
+				</div><!-- /.share-btns -->
+				<button type="button" class="btn btn-success btn-lg" data-dismiss="modal">Close</button>
+			</div><!-- /.modal-body -->
+		</div>
+	</div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+<!-- Share widget make into an include file -->
+
+
+<script src="_assets_/js/jquery.min.js"></script>
+<script src="_assets_/plugins/modernizr/modernizr.custom.js"></script>
+<script src="_assets_/plugins/owl.carousel/owl-carousel/owl.carousel.min.js"></script>
+<script src="_assets_/plugins/jquery.bxslider/jquery.bxslider.min.js"></script>
+<script src="_assets_/plugins/bootstrap/js/bootstrap.min.js"></script>
+<script src="_assets_/plugins/twitter/js/jquery.tweet.min.js"></script>
+<script src="_assets_/plugins/instafeed/instafeed.min.js"></script>
+<script src="_assets_/plugins/sticky/jquery.sticky.js"></script>
+<script src="_assets_/plugins/matchHeight/dist/jquery.matchHeight-min.js"></script>
+<script src="_assets_/plugins/revizeWeather/js/revizeWeather.min.js"></script>
+<script src="_assets_/plugins/waypoints/waypoints.min.js"></script>
+<script src="_assets_/js/scripts.js"></script>
+
+</body>
+</html>
